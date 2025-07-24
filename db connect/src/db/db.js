6091390bcb9b213f0f0115cf2ee916c94a35da42
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-07-24 23:54:07 +0530

    db connect

---
+++ db connect/src/db/db.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const mongoose = require("mongoose");
+
+
+function connectDB(){
+    mongoose.connect(`mongodb+srv://mern14:${process.env.PASSWORD}@cluster0.presvox.mongodb.net/node`)
+    .then(()=>{
+        console.log("DB is connected")
+    })
+    .catch((err) =>{
+      console.log(err)
+    })
+}
+
+module.exports = connectDB;
--- project1/src/app.js
+++ project1/src/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
-
 const notes = [];
 app.post("/notes",(req,res)=>{
     console.log(req.body)
@@ -12,32 +9,23 @@ app.post("/notes",(req,res)=>{
         message:"notes created successfully"
     })
 })
-
 app.get("/notes",(req,res)=>{
     res.status(200).json({
         message:"notes fetch successfully",
         notes
     })
-   
-})
-
-/*DELETE method*/
-
+   })
 app.delete("/notes/:index", (req, res)=>{
     const index = req.params.index
-
     delete notes[index];
-
     res.status(200).json({
         message: "notes deleted successfully"
     })
 })
-
 app.patch("/notes/:index", (req,res)=>{
     const index = req.params.index
     const {title} = req.body;
     notes[index].title = title
-
     res.status(200).json({
         message : "notes updated successfully"
     })

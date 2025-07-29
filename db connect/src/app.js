3ebfd93e7159e30fa06ed387d4063070935b7e58
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-07-29 23:17:37 +0530

    crud

--- db connect/src/app.js
+++ db connect/src/app.js
@@ -15,5 +15,44 @@ app.post("/notes",async (req, res)=>{
     })
 })
 
+// app.get("/notes", async(req, res)=>{
+//     const notes = await notesModel.find()
+//     res.status(200).json({
+//         message: "notes fetched successfully",
+//         notes
+//     })
+// })
+
+app.get("/notes", async(req, res)=>{
+    const notes = await notesModel.find({});
+        res.status(200).json({
+            message : "notes fetched successfully",
+            notes   
+        })
+    
+})
+
+app.delete("/notes/:id", async(req, res)=>{
+    const id  = req.params.id
+  const notes=  await notesModel.findOneAndDelete({
+        _id : id
+    })
+
+    res.status(200).json({
+        message:' note deleted successfully',
+        notes
+    })
+})
+
+app.patch("/notes/:id", async(req, res)=>{
+    const id = req.params.id
+    const {title} = req.body
+    await notesModel.findOneAndUpdate({_id: id},{title})
+    res.status(200).json({
+    message : "notes updated successfully"
+})
+})
+
+
 
 module.exports = app;

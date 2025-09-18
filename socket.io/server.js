7a64ea253caf2110cbd658110f07dcd759a8fdd4
Mistrygeeta <geeta.mistry2001@gmail.com>
2025-09-18 22:40:49 +0530

    implemet socket io

--- socket.io/server.js
+++ socket.io/server.js
@@ -7,17 +7,20 @@ const server = http.createServer(app)
 const io = socketIo(server)
 app.get("/",(req, res)=>{
     res.render("index.ejs",{greet : "good evening"})
-})
+});
+
 
 io.on("connect",(socket)=>{
     console.log("a user connected", socket.id)
 
     socket.on("chat",(msg)=>{
         console.log("msg received-->", msg)
-    })
+    });
 
     socket.emit("chat","i'm good")
-})
+});
+
+
 server.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
